74b9d6d8af3986e9ba4937653fe0398f74c325a8
Srost <Srost@R-COMP7>
2012-08-04 19:00:48 +0400

    *

--- Setup/install.bat
+++ Setup/install.bat
@@ -2,4 +2,4 @@ xcopy Hunspellx86.dll "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Commo
 xcopy Hunspellx64.dll "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\" /Y
 xcopy Hunspellx86.dll "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\" /Y
 xcopy Hunspellx64.dll "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\" /Y
-start SpellChecker.vsix
+start Hunspellchecker.vsix
